a6eb85a65dd0cb26e5da138b97b9c2e12f7aa158
rameez <rameez@192.168.1.28>
2024-11-04 19:01:07 +0500

    add create and update blog functionality

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,4 +5,32 @@ class BlogsController < ApplicationController
   def show
     @blog = Blog.find(params[:id])
   end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def create
+    @blog = Blog.new(params.require(:blog).permit(:title, :description))
+    if @blog.save
+      flash[:notice] = "Blog is created successfully."
+      redirect_to blog_path(@blog)
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(params.require(:blog).permit(:title, :description))
+      flash[:notice] = "Blog is updated successfully."
+      redirect_to blog_path(@blog)
+    else
+      render "edit"
+    end
+  end
 end
